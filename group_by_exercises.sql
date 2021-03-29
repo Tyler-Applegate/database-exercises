8c82748ff88e6a6c082a8f1844e81e1c5c6f28e8
Tyler-Applegate <tyler.applegate@gmail.com>
2021-03-29 12:10:16 -0500

    Made group_by_exercises template

---
+++ group_by_exercises.sql
@@ -0,0 +1,25 @@
+group_by_exercises
+
+1.  Create a new file named group_by_exercises.sql
+
+2.  In your script, use DISTINCT to find the unique titles in the titles table. 
+    How many unique titles have there ever been? Answer that in a comment in your SQL file.
+
+3.  Write a query to to find a list of all unique last names of all employees that start and 
+    end with 'E' using GROUP BY.
+
+4.  Write a query to to find all unique combinations of first and last names of all employees whose 
+    last names start and end with 'E'.
+
+5.  Write a query to find the unique last names with a 'q' but not 'qu'. Include those names in a 
+    comment in your sql code.
+
+6.  Add a COUNT() to your results (the query above) and use ORDER BY to make it easier to find 
+    employees whose unusual name is shared with others.
+
+7.  Find all all employees with first names 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and GROUP BY 
+    to find the number of employees for each gender with those names.
+
+8.  Using your query that generates a username for all of the employees, generate a count employees 
+    for each unique username. Are there any duplicate usernames? BONUS: How many duplicate usernames 
+    are there?
